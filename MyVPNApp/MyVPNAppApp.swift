c77b7ddc0157363f7b23dfa721fff05d7368b7b8
Jay Patel <j012.patel@gmail.com>
2024-08-21 21:04:05 -0400

    Initial Commit

---
+++ MyVPNApp/MyVPNAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVPNAppApp.swift
+//  MyVPNApp
+//
+//  Created by Jay on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MyVPNAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
